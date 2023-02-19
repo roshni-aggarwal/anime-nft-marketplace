5cfe737bce82823e4ffeb3fa9a4c838cc2cc2360
roshiii24 <rrsaggarwal@gmail.com>
2023-02-19 09:15:00 +0530

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,8 +8,9 @@ const Navbar = () => {
     image: "/images/user.png",
   };
   const [connected, toggleConnect] = useState(false);
-   const location = useLocation();
-   const [currAddress, updateAddress] = useState("0x");
+  const location = useLocation();
+  const [currAddress, updateAddress] = useState("0x");
+  const marketPathName = useLocation().pathname.includes("/market");
   return (
     <nav className="flex items-center justify-between px-4 md:px-24 lg:px-24 py-4 shadow sticky top-0 z-50 bg-white">
       <div className="flex items-center gap-2">
@@ -40,11 +41,15 @@ const Navbar = () => {
         </div>
       </div>
       <div className="flex items-center gap-5">
-        <button className="px-4 py-2 border-2 border-r-4 border-b-4 border-black bg-blue-400 font-medium shadow-lg">{connected? "Connected":"Connect Wallet"}
+        <button className="px-4 py-2 border-2 border-r-4 border-b-4 border-black bg-blue-400 font-medium shadow-lg">
+          {connected ? "Connected" : "Connect Wallet"}
         </button>
         <Link to={"/profile"} className="flex items-center">
           <div className="w-10 rounded-full">
-          {currAddress !== "0x" ? "Connected to":alert("Not Connected. Please login to view NFTs")} {currAddress !== "0x" ? (currAddress.substring(0,15)+'...'):""}
+            {/* {currAddress !== "0x"
+              ? "Connected to"
+              : alert("Not Connected. Please login to view NFTs")}{" "}
+            {currAddress !== "0x" ? currAddress.substring(0, 15) + "..." : ""} */}
             <img src={user.image} alt="avatar" className="w-full h-full" />
           </div>
           <h3>{user.name}</h3>
